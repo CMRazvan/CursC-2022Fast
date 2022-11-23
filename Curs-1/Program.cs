628e27d36a2fa49756025ecde374f8519686d963
Costea Mihai Razvan <costeamihairazvan@outlook.com>
2022-11-23 20:28:57 +0200

    Start

---
+++ Curs-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curs_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
