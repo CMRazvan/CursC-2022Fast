d204e20d9fbbd3a63816a6ef490765793c1e9900
Costea Mihai Razvan <costeamihairazvan@outlook.com>
2022-11-23 20:34:04 +0200

    Modificat

---
+++ Curs2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Curs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 3;
+            var y = 2;
+
+            int p = x + y;
+        }
+    }
+}
