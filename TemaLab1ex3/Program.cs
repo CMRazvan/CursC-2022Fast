f0a7faf06b3a48a1936308a801fc016c65ad7978
Costea Mihai Razvan <costeamihairazvan@outlook.com>
2022-11-25 00:40:28 +0200

    Rezolvarea exercitiu 3

---
+++ TemaLab1ex3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TemaLab1ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex3
+            //Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+            //tastaura
+
+            Console.WriteLine("Bine ai venit la Curs C# , acum rezolvam temele:");
+            Console.WriteLine("Exercitiul 3");
+            Console.WriteLine("Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la");
+            Console.WriteLine("tastatura");
+
+            int numarul;
+
+            numarul = int.Parse(Console.ReadLine());
+
+            int lastdigit = (numarul % 10);
+
+            Console.WriteLine(lastdigit);
+
+        }
+    }
+}
