e4e23e06ab66c8bfd627f19c883a291bef618412
Costea Mihai Razvan <costeamihairazvan@outlook.com>
2022-11-25 00:57:32 +0200

    Rezolvarea exercitiului 5

---
+++ TemaLab1ex5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TemaLab1ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex5
+            //Se citesc doua numere de la tastatura, x,y.Scrieti un program care va afisa cele doua valori in ordine crescatoare.
+            //• Exemplu: citim ,9,0 Afisam: 0 9
+
+            Console.WriteLine("Bine ai venit la Curs C# , acum rezolvam temele:");
+            Console.WriteLine("Exercitiul 5");
+            Console.WriteLine("Se citesc doua numere de la tastatura, x,y.Scrieti un program care va afisa cele doua valori in ordine crescatoare.");
+            Console.WriteLine("• Exemplu: citim ,9,0 Afisam: 0 9");
+
+
+            Console.WriteLine("Tasteaza primul numar:");
+
+                int x;
+                x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tasteaza al doilea numar:");
+
+                int y;
+                y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine("Ordinea numerelor este: " + y + ", " + x);
+            }
+            else
+            {
+                Console.WriteLine("Ordinea numerelor este: " + x + ", " + y);
+
+            }
+
+        }
+    }
+}
