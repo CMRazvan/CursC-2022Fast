b856a97e81606d7a11aba7c2fce15776dfe8b2eb
Costea Mihai Razvan <costeamihairazvan@outlook.com>
2022-11-25 19:41:03 +0200

    Corectare tree

---
+++ TemaLab1ex7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemaLab1ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
