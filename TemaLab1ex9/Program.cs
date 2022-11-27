5f60f1ada880e3e5b9a777689513cf615196f38f
Costea Mihai Razvan <costeamihairazvan@outlook.com>
2022-11-27 22:24:10 +0200

    Rezolvare exercitiu 9

--- TemaLab1ex9/Program.cs
+++ TemaLab1ex9/Program.cs
@@ -14,7 +14,24 @@ namespace TemaLab1ex9
             Console.WriteLine("Scrieti un program care interschimba valoarea a doua variabile intregi.");
 
 
+            int a;
+            int b; 
+            int schimb;
 
+            Console.Write("Introduceti primul numar a = ");
+            a = int.Parse(Console.ReadLine());
+            
+            Console.Write("Introduceti al doilea numar b = ");
+            b = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("a=" + a + " , b=" + b);
+            
+            schimb = a;
+            a = b;
+            b = schimb;
+            
+            Console.Write("Numerele inlocuite sunt a=" + a + " b=" + b);
+            Console.ReadKey();
 
         }
     }
